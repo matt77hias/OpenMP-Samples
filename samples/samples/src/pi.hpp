06a81524c56abf6536becef37c589d96c3da5d84
ALIEN <matthias.moulin@gmail.com>
2016-12-05 20:52:04 +0100

    Added critical section example

--- samples/samples/src/pi.hpp
+++ samples/samples/src/pi.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+// sequential pi calculation
 inline double pi_sequential(int nb_blocks = 1000) {
 
 	const double dx = 1.0 / nb_blocks;
@@ -16,6 +17,7 @@ inline double pi_sequential(int nb_blocks = 1000) {
 	return 4.0 * integral;
 }
 
+// parallel pi calculation with reduction directive
 inline double pi_parallel(int nb_blocks = 1000) {
 
 	const double dx = 1.0 / nb_blocks;
@@ -27,6 +29,25 @@ inline double pi_parallel(int nb_blocks = 1000) {
 		const double b = a + dx;
 		const double fa = 1.0 / (1.0 + a*a);
 		const double fb = 1.0 / (1.0 + b*b);
+		integral += 0.5 * dx * (fa + fb);
+	}
+
+	return 4.0 * integral;
+}
+
+// parallel pi calculation with critical directive
+inline double pi_parallel_no_reduction(int nb_blocks = 1000) {
+
+	const double dx = 1.0 / nb_blocks;
+
+	double integral = 0.0;
+#pragma omp parallel for schedule(static) default(none) firstprivate(nb_blocks) shared(integral)
+	for (int i = 0; i < nb_blocks; ++i) {
+		const double a = i * dx;
+		const double b = a + dx;
+		const double fa = 1.0 / (1.0 + a*a);
+		const double fb = 1.0 / (1.0 + b*b);
+#pragma omp critical
 		integral += 0.5 * dx * (fa + fb);
 	}
 
--- samples/samples/src/samples.cpp
+++ samples/samples/src/samples.cpp
@@ -11,5 +11,9 @@ int main() {
 	printf("pi: %lf\n", pi_parallel(1000));
 	printf("pi: %lf\n", pi_parallel(10000));
 
+	printf("pi: %lf\n", pi_parallel_no_reduction(100));
+	printf("pi: %lf\n", pi_parallel_no_reduction(1000));
+	printf("pi: %lf\n", pi_parallel_no_reduction(10000));
+
 	return 0;
 }
