c09ab29ccf66e803883751916bdfe86a2a28972e
ALIEN <matthias.moulin@gmail.com>
2016-12-05 17:37:47 +0100

    Refactor

---
+++ samples/samples/src/pi.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+inline double pi_sequential(int nb_blocks = 1000) {
+
+	const double dx = 1.0 / nb_blocks;
+
+	double integral = 0.0;
+	for (int i = 0; i < nb_blocks; ++i) {
+		const double a = i * dx;
+		const double b = a + dx;
+		const double fa = 1.0 / (1.0 + a*a);
+		const double fb = 1.0 / (1.0 + b*b);
+		integral += 0.5 * dx * (fa + fb);
+	}
+
+	return 4.0 * integral;
+}
+
+inline double pi_parallel(int nb_blocks = 1000) {
+
+	const double dx = 1.0 / nb_blocks;
+
+	double integral = 0.0;
+#pragma omp parallel for schedule(static) default(none) firstprivate(nb_blocks) reduction(+:integral)
+	for (int i = 0; i < nb_blocks; ++i) {
+		const double a = i * dx;
+		const double b = a + dx;
+		const double fa = 1.0 / (1.0 + a*a);
+		const double fb = 1.0 / (1.0 + b*b);
+		integral += 0.5 * dx * (fa + fb);
+	}
+
+	return 4.0 * integral;
+}
