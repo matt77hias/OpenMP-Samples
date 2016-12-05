4a406c528b668dc01bcb843d670a8741a4e8ef66
ALIEN <matthias.moulin@gmail.com>
2016-12-05 20:46:45 +0100

    Update

--- samples/samples/src/samples.cpp
+++ samples/samples/src/samples.cpp
@@ -3,7 +3,13 @@
 
 int main() {
 
+	printf("pi: %lf\n", pi_sequential(100));
+	printf("pi: %lf\n", pi_sequential(1000));
+	printf("pi: %lf\n", pi_sequential(10000));
+
+	printf("pi: %lf\n", pi_parallel(100));
 	printf("pi: %lf\n", pi_parallel(1000));
+	printf("pi: %lf\n", pi_parallel(10000));
 
 	return 0;
 }
