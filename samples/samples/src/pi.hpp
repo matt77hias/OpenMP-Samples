de8308ec8a5cd14cbb0fc813fc47d01779249b63
Matthias Moulin <matthias.moulin@gmail.com>
2018-06-01 16:20:59 +0200

    Update pi.hpp

--- samples/samples/src/pi.hpp
+++ samples/samples/src/pi.hpp
@@ -23,7 +23,7 @@ inline double pi_parallel(int nb_blocks = 1000) noexcept {
 	const double dx = 1.0 / nb_blocks;
 
 	double integral = 0.0;
-#pragma omp parallel for schedule(static) default(none) firstprivate(nb_blocks) reduction(+:integral)
+	#pragma omp parallel for schedule(static) default(none) firstprivate(nb_blocks) reduction(+:integral)
 	for (int i = 0; i < nb_blocks; ++i) {
 		const double a = i * dx;
 		const double b = a + dx;
@@ -41,13 +41,13 @@ inline double pi_parallel_no_reduction(int nb_blocks = 1000) noexcept {
 	const double dx = 1.0 / nb_blocks;
 
 	double integral = 0.0;
-#pragma omp parallel for schedule(static) default(none) firstprivate(nb_blocks) shared(integral)
+	#pragma omp parallel for schedule(static) default(none) firstprivate(nb_blocks) shared(integral)
 	for (int i = 0; i < nb_blocks; ++i) {
 		const double a = i * dx;
 		const double b = a + dx;
 		const double fa = 1.0 / (1.0 + a*a);
 		const double fb = 1.0 / (1.0 + b*b);
-#pragma omp critical
+		#pragma omp critical
 		integral += 0.5 * dx * (fa + fb);
 	}
 
