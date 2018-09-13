05391aa8e1f87bfc0bff0f8d19932867764b335d
REDDRAGON <matthias.moulin@gmail.com>
2018-09-13 14:35:33 +0200

    Refactor

--- samples/samples/src/pi.hpp
+++ samples/samples/src/pi.hpp
@@ -1,55 +1,55 @@
 #pragma once
 
 // sequential pi calculation
-inline double pi_sequential(int nb_blocks = 1000) noexcept {
+inline double pi_sequential(int nb_steps) noexcept {
 
-	const double dx = 1.0 / nb_blocks;
+	const double dx = 1.0 / nb_steps;
 
 	double integral = 0.0;
-	for (int i = 0; i < nb_blocks; ++i) {
-		const double a = i * dx;
-		const double b = a + dx;
+	for (int i = 0; i < nb_steps; ++i) {
+		const double a  = i * dx;
+		const double b  = a + dx;
 		const double fa = 1.0 / (1.0 + a*a);
 		const double fb = 1.0 / (1.0 + b*b);
-		integral += 0.5 * dx * (fa + fb);
+		integral += fa + fb;
 	}
 
-	return 4.0 * integral;
+	return 2.0 * dx * integral;
 }
 
 // parallel pi calculation with reduction directive
-inline double pi_parallel(int nb_blocks = 1000) noexcept {
+inline double pi_parallel(int nb_steps) noexcept {
 
-	const double dx = 1.0 / nb_blocks;
+	const double dx = 1.0 / nb_steps;
 
 	double integral = 0.0;
-	#pragma omp parallel for schedule(static) default(none) firstprivate(nb_blocks) reduction(+:integral)
-	for (int i = 0; i < nb_blocks; ++i) {
-		const double a = i * dx;
-		const double b = a + dx;
+	#pragma omp parallel for schedule(static) default(none) firstprivate(nb_steps) reduction(+:integral)
+	for (int i = 0; i < nb_steps; ++i) {
+		const double a  = i * dx;
+		const double b  = a + dx;
 		const double fa = 1.0 / (1.0 + a*a);
 		const double fb = 1.0 / (1.0 + b*b);
-		integral += 0.5 * dx * (fa + fb);
+		integral += fa + fb;
 	}
 
-	return 4.0 * integral;
+	return 2.0 * dx * integral;
 }
 
 // parallel pi calculation with critical directive
-inline double pi_parallel_no_reduction(int nb_blocks = 1000) noexcept {
+inline double pi_parallel_no_reduction(int nb_steps) noexcept {
 
-	const double dx = 1.0 / nb_blocks;
+	const double dx = 1.0 / nb_steps;
 
 	double integral = 0.0;
-	#pragma omp parallel for schedule(static) default(none) firstprivate(nb_blocks) shared(integral)
-	for (int i = 0; i < nb_blocks; ++i) {
-		const double a = i * dx;
-		const double b = a + dx;
+	#pragma omp parallel for schedule(static) default(none) firstprivate(nb_steps) shared(integral)
+	for (int i = 0; i < nb_steps; ++i) {
+		const double a  = i * dx;
+		const double b  = a + dx;
 		const double fa = 1.0 / (1.0 + a*a);
 		const double fb = 1.0 / (1.0 + b*b);
 		#pragma omp critical
-		integral += 0.5 * dx * (fa + fb);
+		integral += fa + fb;
 	}
 
-	return 4.0 * integral;
+	return 2.0 * dx * integral;
 }
--- samples/samples/src/samples.cpp
+++ samples/samples/src/samples.cpp
@@ -1,20 +1,35 @@
-#include "targetver.hpp"
-#include <stdio.h>
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#pragma region
+
 #include "pi.hpp"
 
+#pragma endregion
+//-----------------------------------------------------------------------------
+// System Includes
+//-----------------------------------------------------------------------------
+#pragma region
+
+#include <iostream>
+#include <limits>
+
+#pragma endregion
+
 int main() {
+	constexpr int a[] = { 100, 1000, 10000 };
 
-	printf("pi: %lf\n", pi_sequential(100));
-	printf("pi: %lf\n", pi_sequential(1000));
-	printf("pi: %lf\n", pi_sequential(10000));
+	std::cout.precision(std::numeric_limits< double >::max_digits10);
 
-	printf("pi: %lf\n", pi_parallel(100));
-	printf("pi: %lf\n", pi_parallel(1000));
-	printf("pi: %lf\n", pi_parallel(10000));
-
-	printf("pi: %lf\n", pi_parallel_no_reduction(100));
-	printf("pi: %lf\n", pi_parallel_no_reduction(1000));
-	printf("pi: %lf\n", pi_parallel_no_reduction(10000));
+	for (int nb_steps : a) {
+		std::cout << "pi: " << pi_sequential(nb_steps) << std::endl;
+	}
+	for (int nb_steps : a) {
+		std::cout << "pi: " << pi_parallel(nb_steps) << std::endl;
+	}
+	for (int nb_steps : a) {
+		std::cout << "pi: " << pi_parallel_no_reduction(nb_steps) << std::endl;
+	}
 
 	return 0;
 }
