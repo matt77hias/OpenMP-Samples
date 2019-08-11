920bf099b4b91ebc9a92cc7cb41c07298b376b1e
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 09:11:40 +0200

    Update samples.cpp

--- samples/samples/src/samples.cpp
+++ samples/samples/src/samples.cpp
@@ -1,34 +1,36 @@
 //-----------------------------------------------------------------------------
 // Includes
 //-----------------------------------------------------------------------------
-#pragma region
 
+// pi_parallel, pi_parallel_no_reduction, pi_sequential
 #include "pi.hpp"
 
-#pragma endregion
 //-----------------------------------------------------------------------------
 // System Includes
 //-----------------------------------------------------------------------------
-#pragma region
 
+// cout
 #include <iostream>
+// numeric_limits
 #include <limits>
 
-#pragma endregion
-
-int main() {
+int main()
+{
 	constexpr int a[] = { 100, 1000, 10000 };
 
 	std::cout.precision(std::numeric_limits< double >::max_digits10);
 
-	for (int nb_steps : a) {
-		std::cout << "pi: " << pi_sequential(nb_steps) << std::endl;
+	for (int step_count : a)
+	{
+		std::cout << "pi: " << pi_sequential(step_count) << std::endl;
 	}
-	for (int nb_steps : a) {
-		std::cout << "pi: " << pi_parallel(nb_steps) << std::endl;
+	for (int step_count : a)
+	{
+		std::cout << "pi: " << pi_parallel(step_count) << std::endl;
 	}
-	for (int nb_steps : a) {
-		std::cout << "pi: " << pi_parallel_no_reduction(nb_steps) << std::endl;
+	for (int step_count : a)
+	{
+		std::cout << "pi: " << pi_parallel_no_reduction(step_count) << std::endl;
 	}
 
 	return 0;
