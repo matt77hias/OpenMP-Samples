18e941ef9cb13d2296ffd656445ebd79c15c20c9
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 09:09:11 +0200

    Update pi.hpp

--- samples/samples/src/pi.hpp
+++ samples/samples/src/pi.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 // sequential pi calculation
-inline double pi_sequential(int nb_steps) noexcept {
-
-	const double dx = 1.0 / nb_steps;
+inline double pi_sequential(int step_count) noexcept
+{
+	const double dx = 1.0 / step_count;
 
 	double integral = 0.0;
-	for (int i = 0; i < nb_steps; ++i) {
+	for (int i = 0; i < step_count; ++i)
+	{
 		const double a  = i * dx;
 		const double b  = a + dx;
 		const double fa = 1.0 / (1.0 + a*a);
@@ -18,13 +19,15 @@ inline double pi_sequential(int nb_steps) noexcept {
 }
 
 // parallel pi calculation with reduction directive
-inline double pi_parallel(int nb_steps) noexcept {
+inline double pi_parallel(int step_count) noexcept
+{
 
-	const double dx = 1.0 / nb_steps;
+	const double dx = 1.0 / step_count;
 
 	double integral = 0.0;
-	#pragma omp parallel for schedule(static) default(none) firstprivate(nb_steps) reduction(+:integral)
-	for (int i = 0; i < nb_steps; ++i) {
+	#pragma omp parallel for schedule(static) default(none) firstprivate(step_count) reduction(+:integral)
+	for (int i = 0; i < step_count; ++i)
+	{
 		const double a  = i * dx;
 		const double b  = a + dx;
 		const double fa = 1.0 / (1.0 + a*a);
@@ -36,13 +39,15 @@ inline double pi_parallel(int nb_steps) noexcept {
 }
 
 // parallel pi calculation with critical directive
-inline double pi_parallel_no_reduction(int nb_steps) noexcept {
+inline double pi_parallel_no_reduction(int step_count) noexcept
+{
 
-	const double dx = 1.0 / nb_steps;
+	const double dx = 1.0 / step_count;
 
 	double integral = 0.0;
-	#pragma omp parallel for schedule(static) default(none) firstprivate(nb_steps) shared(integral)
-	for (int i = 0; i < nb_steps; ++i) {
+	#pragma omp parallel for schedule(static) default(none) firstprivate(step_count) shared(integral)
+	for (int i = 0; i < step_count; ++i)
+	{
 		const double a  = i * dx;
 		const double b  = a + dx;
 		const double fa = 1.0 / (1.0 + a*a);
